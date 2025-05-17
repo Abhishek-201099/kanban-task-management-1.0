e098e8c74bacb452ec0ccf462e9cafe634ba26c2
Abhishek Bhattacharjee <ahishek1250@gmail.com>
2025-05-17 14:32:46 +0530

    Error and not-found fix

--- app/error.jsx
+++ app/error.jsx
@@ -1,5 +1,22 @@
 "use client";
 
+import { ArrowLeftIcon } from "@heroicons/react/24/solid";
+import Link from "next/link";
+
 export default function Error({ error, reset }) {
-  return <div>{error.message}</div>;
+  return (
+    <main className="flex justify-center items-center flex-col gap-6 h-screen">
+      <h1 className="text-3xl font-semibold">Something went wrong!</h1>
+
+      <Link
+        href="/boards"
+        className="bg-accent-500 text-primary-800 px-6 py-3 text-base lg:text-lg flex items-center gap-4"
+      >
+        <span>
+          <ArrowLeftIcon className="h-4 w-4" />
+        </span>
+        <span className="font-semibold">Go back</span>
+      </Link>
+    </main>
+  );
 }
--- app/not-found.jsx
+++ app/not-found.jsx
@@ -1,3 +1,25 @@
+import { ArrowLeftIcon } from "@heroicons/react/24/solid";
+import Link from "next/link";
+
 export default function notFound() {
-  return <div>Not found ....</div>;
+  return (
+    <main className="flex justify-center items-center flex-col gap-6 h-screen [word-spacing:4px]">
+      <h1 className="text-base md:text-xl lg:text-3xl font-semibold uppercase">
+        Not found
+      </h1>
+      <p className="text-xs md:text-sm lg:text-base text-primary-400">
+        Could not find requested resource
+      </p>
+
+      <Link
+        href="/boards"
+        className="bg-accent-500 text-primary-800 px-6 py-3 text-base lg:text-lg flex items-center gap-4"
+      >
+        <span>
+          <ArrowLeftIcon className="h-4 w-4" />
+        </span>
+        <span className="font-semibold">Go back</span>
+      </Link>
+    </main>
+  );
 }
